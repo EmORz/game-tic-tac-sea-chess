89d69015f6d8fd84dd1ca8f0abc370808519a4e3
Emo <eiv.razgrad@gmail.com>
2018-09-17 18:56:35 +0300

    Simple game

---
+++ TicTac/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTac
+{
+    public partial class Form1 : Form
+    {
+        bool turn = true; // true>X; false>Y
+        int turn_count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("By Emo", "Tic Tac Toe About");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (turn)            
+                b.Text = "X";            
+            else            
+                b.Text = "O";            
+            turn = !turn;
+            b.Enabled = false;
+            turn_count++;
+            CheckWiner();
+        }
+        private void CheckWiner()
+        {
+            bool ThereIsWiner = false;
+            //horizontal
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+                ThereIsWiner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+                ThereIsWiner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+                ThereIsWiner = true;
+            //vertical
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+                ThereIsWiner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+                ThereIsWiner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+                ThereIsWiner = true;
+            //diagonal
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+                ThereIsWiner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+                ThereIsWiner = true;
+
+            if (ThereIsWiner)
+            {
+                diasbleButtons();
+                String win = "";
+                if (turn)
+                    win = "O";
+                else
+                    win = "X";
+                MessageBox.Show(win + " Wins!", "Yea!");
+            }//End if
+            else
+            {
+                if (turn_count==9)
+                {
+                    MessageBox.Show("Draw!", "Bummer!");
+                }
+            }
+        }//End ChekWiner
+        private void diasbleButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch { }
+
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            turn = true;
+            turn_count = 0;
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+            }
+            catch { }
+
+        }
+    }
+
+}
